e79bccd5b77be7d528751deb3cbc3edc39e643ce
oink24 <oooq3o@gmail.com>
2023-09-14 17:39:09 +0900

    기능구현 - 관리자/상품등록ing

--- Kmarket/src/main/java/kr/co/kmarket/controller/admin/product/RegisterController.java
+++ Kmarket/src/main/java/kr/co/kmarket/controller/admin/product/RegisterController.java
@@ -1,6 +1,9 @@
 package kr.co.kmarket.controller.admin.product;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,15 +12,37 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import kr.co.kmarket.dto.ProductCate2DTO;
+import kr.co.kmarket.service.ProductCate2Service;
+
 @WebServlet("/admin/product/register.do")
 public class RegisterController extends HttpServlet{
-
 	private static final long serialVersionUID = -575977800525097241L;
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	private ProductCate2Service service = ProductCate2Service.INSTANCE;
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
 		RequestDispatcher dispatcher = req.getRequestDispatcher("/admin/product/register.jsp");
 		dispatcher.forward(req, resp);
 	}
-
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String cate1 = req.getParameter("cate1");
+		logger.info("cate1 : " + cate1);
+		
+		List<ProductCate2DTO> cate2 = service.selectProductCate2s(cate1);
+		logger.info("cate2 : " + cate2);
+		
+		Map<String, Object> data = new HashMap<>();
+		data.put("cate2", cate2);
+		
+		resp.setContentType("application/x-json; charset=utf-8");
+		resp.getWriter().print(data); // 전송
+	}
 }
