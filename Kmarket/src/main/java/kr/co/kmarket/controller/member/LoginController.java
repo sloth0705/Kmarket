f2e1fee792ffbf9fa52d8f9122976b05b80cd465
oink24 <oooq3o@gmail.com>
2023-09-14 10:27:27 +0900

    기능구현 - 회원/로그인 1차개발

--- Kmarket/src/main/java/kr/co/kmarket/controller/member/LoginController.java
+++ Kmarket/src/main/java/kr/co/kmarket/controller/member/LoginController.java
@@ -8,23 +8,48 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import kr.co.kmarket.dto.MemberDTO;
 import kr.co.kmarket.service.MemberService;
 
 @WebServlet("/member/login.do")
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = -2365079189659779648L;
-	private MemberService servicve = MemberService.INSTANCE;
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	private MemberService service = MemberService.INSTANCE;
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+		String success = req.getParameter("success");
+		req.setAttribute("success", success);
+		
 		RequestDispatcher dispatcher = req.getRequestDispatcher("/member/login.jsp");
 		dispatcher.forward(req, resp);	
 	}
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
+		String uid  = req.getParameter("uid");
+		String pass = req.getParameter("pass");
 		
+		MemberDTO member = service.selectMember(uid, pass); // 사용자가 존재하지 않으면 null
+		
+		if (member != null)
+		{
+			HttpSession session = req.getSession();
+			session.setAttribute("sessMember", member);
+			
+			logger.info("sessMember level : " + member.getLevel());
+			resp.sendRedirect("/Kmarket/index.do");
+		}
+		else
+		{
+			resp.sendRedirect("/Kmarket/member/login.do?success=100");
+		}
 	}
 }
--- Kmarket/src/main/java/kr/co/kmarket/dao/MemberDAO.java
+++ Kmarket/src/main/java/kr/co/kmarket/dao/MemberDAO.java
@@ -74,8 +74,51 @@ public class MemberDAO extends DBHelper {
 		}
 	}
 
-	public MemberDTO selectMember(String uid) {
+	public MemberDTO selectMember(String uid, String pass) {
+		
 		MemberDTO dto = null;
+		try {
+			conn = getConnection();
+			psmt = conn.prepareStatement("SELECT * FROM `km_member` WHERE `uid`=? AND `pass`=SHA2(?, 256)");
+			psmt.setString(1, uid);
+			psmt.setString(2, pass);
+			
+			rs = psmt.executeQuery();
+			
+			if (rs.next())
+			{
+				dto = new MemberDTO();
+				dto.setUid(rs.getString(1));
+				dto.setPass(rs.getString(2));
+				dto.setName(rs.getString(3));
+				dto.setGender(rs.getString(4));
+				dto.setHp(rs.getString(5));
+				dto.setEmail(rs.getString(6));
+				dto.setType(rs.getString(7));
+				dto.setPoint(rs.getInt(8));
+				dto.setLevel(rs.getString(9));
+				dto.setZip(rs.getString(10));
+				dto.setAddr1(rs.getString(11));
+				dto.setAddr2(rs.getString(12));
+				dto.setCompany(rs.getString(13));
+				dto.setCeo(rs.getString(14));
+				dto.setBizRegNum(rs.getString(15));
+				dto.setComRegNum(rs.getString(16));
+				dto.setTel(rs.getString(17));
+				dto.setManager(rs.getString(18));
+				dto.setManagerHp(rs.getString(19));
+				dto.setFax(rs.getString(20));
+				dto.setRegip(rs.getString(21));
+				dto.setWdate(rs.getString(22));
+				dto.setRdate(rs.getString(23));
+			}
+			
+			close();
+			
+		}catch(Exception e) {
+			logger.error("selectMember() error : " + e.getMessage());
+		}
+		
 		return dto;
 	}
 	public int selectCountUid(String uid) {
