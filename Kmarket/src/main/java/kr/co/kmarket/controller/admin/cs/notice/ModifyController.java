c3a2cce72a6692580751a00060da58905fce06f8
oink24 <oooq3o@gmail.com>
2023-09-25 17:27:43 +0900

    기능구현 - 관리자/고객문의/공지사항ing

--- Kmarket/src/main/java/kr/co/kmarket/controller/admin/cs/notice/ModifyController.java
+++ Kmarket/src/main/java/kr/co/kmarket/controller/admin/cs/notice/ModifyController.java
@@ -26,17 +26,17 @@ public class ModifyController extends HttpServlet {
 		
 		String type = req.getParameter("type");
 		String cate = req.getParameter("cate");
-		String no   = req.getParameter("no");
-		CS_BoardDTO dto = service.selectCS_Board(Integer.parseInt(no));
+		String bno  = req.getParameter("bno");
+		CS_BoardDTO dto = service.selectCS_Board(Integer.parseInt(bno));
 		
 		req.setAttribute("type", type);
-		req.setAttribute("no", no);
+		req.setAttribute("bno", bno);
 		req.setAttribute("cate", cate);
 		req.setAttribute("title", dto.getTitle());
 		req.setAttribute("content", dto.getContent());
 		
 		logger.info("doGet type : " + type);
-		logger.info("doGet no : " + no);
+		logger.info("doGet bno : " + bno);
 		
 		RequestDispatcher dispatcher = req.getRequestDispatcher("/admin/cs/notice/modify.jsp");
 		dispatcher.forward(req, resp);
@@ -44,16 +44,20 @@ public class ModifyController extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		String type    = req.getParameter("type");
-		String no      = req.getParameter("no");
+		String strBno  = req.getParameter("bno");
+		String strType = req.getParameter("type");
 		String cate    = req.getParameter("cate");
 		String title   = req.getParameter("title");
 		String content = req.getParameter("content");
-		logger.info("작성할 cate : " + cate);
+		logger.info("doPost cate : " + cate);
+		logger.info("doPost bno : " + strBno);
+		logger.info("doPost type : " + strType);
+		int type = Integer.parseInt(strType);
+		int bno  = Integer.parseInt(strBno);
 		
 		CS_BoardDTO dto = new CS_BoardDTO();
-		dto.setType(Integer.parseInt(type));
-		dto.setBno(Integer.parseInt(no));
+		dto.setType(type);
+		dto.setBno(bno);
 		dto.setCate(cate);
 		dto.setTitle(title);
 		dto.setContent(content);
